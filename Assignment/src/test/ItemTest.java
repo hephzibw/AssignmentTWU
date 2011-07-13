276047eb3185827ac01431627f3824e6bfd6ddf2
Hephzibah <hephzibah.1989@gmail.com>
2011-07-13 10:23:54 +0530

    some more refactoring

--- Assignment/src/test/ItemTest.java
+++ Assignment/src/test/ItemTest.java
@@ -14,45 +14,75 @@ public class ItemTest {
         //Arrange
         Item item = new Item("Box of Chocolates", 10.0, true);
 
-
+        //Act
         String name = item.getName();
         double shelfPrice = item.getShelfPrice();
         boolean imported = item.getImported();
+        
+        //Assert
         assertThat("Box of Chocolates", is(name));
-
         assertThat(10.0, is(shelfPrice));
-
         assertThat(true, is(imported));
     }
 
     @Test
     public void shouldReturnTheValueForExemptedAsTrueForBook() {
+        //Arrange
         Item item = new Item("book", 12.45, false);
-        assertThat(true, is(item.checkWhetherItemIsBasicSalesTaxExemptableOrNot()));
+        
+        //Act
+        boolean value = item.checkWhetherItemIsBasicSalesTaxExemptableOrNot();
+        
+        //Assert
+        assertThat(true, is(value));
     }
 
     @Test
     public void shouldReturnTheValueForExemptedAsFalseForMusicCD() {
+        //Arrange
         Item item = new Item("music CD", 12.0, false);
-        assertThat(false, is(item.checkWhetherItemIsBasicSalesTaxExemptableOrNot()));
+        
+        //Act
+        boolean value = item.checkWhetherItemIsBasicSalesTaxExemptableOrNot();
+        
+        //Assert
+        assertThat(false, is(value));
     }
 
     @Test
     public void shouldReturnTheValueForExemptedAsFalseForBottleOfPerfume() {
+        //Arrange
         Item item = new Item("bottle of perfume", 12.45, false);
-        assertThat(false, is(item.checkWhetherItemIsBasicSalesTaxExemptableOrNot()));
+        
+        //Act
+        boolean value = item.checkWhetherItemIsBasicSalesTaxExemptableOrNot();
+        
+        //Assert
+        assertThat(false, is(value));
     }
 
     @Test
     public void shouldReturnTheValueForExemptedAsTrueForImportedBoxOfChocolates() {
+        //Arrange
         Item item = new Item("imported box of chocolates", 60.56, true);
-        assertThat(true, is(item.checkWhetherItemIsBasicSalesTaxExemptableOrNot()));
+        
+        //Act
+        boolean value = item.checkWhetherItemIsBasicSalesTaxExemptableOrNot();
+        
+        //Assert
+        assertThat(true, is(value));
     }
 
     @Test
     public void shouldCalculateCostOfImportedBottleOfPerfumeCosting27Point99As32Point19(){
+        //Arrange
         Item item=new Item("imported bottle of perfume",27.99,true);
-        assertThat(32.19, is(item.getCostOfItem()));
+        
+        //Act
+        double cost = item.getCostOfItem();
+        
+        //Assert
+        assertThat(32.19, is(cost));
 
     }
 
